278f0f6e01a67bbe45604245f229567ed94cc241
Sergey Ryabov <sergey.ryabov@actum.cz>
2017-11-05 10:35:06 +0100

    Add Plumber to tpl compilation task

--- gulp/tasks/tpl.js
+++ gulp/tasks/tpl.js
@@ -3,11 +3,13 @@ const gulp = require('gulp');
 const nunjucks = require('gulp-nunjucks');
 const rename = require('gulp-rename');
 const browserSync = require('browser-sync');
+const plumber = require('gulp-plumber');
 
 const { BUILD_BASE, DEV_BASE, TPL_FILES } = config;
 
 gulp.task('tpl-compile', () =>
     gulp.src(TPL_FILES)
+        .pipe(plumber())
         .pipe(nunjucks.compile())
         .pipe(rename((path) => {
             path.extname = '.html'
