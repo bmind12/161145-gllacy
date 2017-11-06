41f205b98c306ccb0bb82f15e0494a76b349c0a1
Sergey Ryabov <sergey.ryabov@actum.cz>
2017-11-06 08:24:14 +0100

    Add watching for newly created / deleted files

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -1,6 +1,8 @@
 const config = require('../config');
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
+const gwatch = require('gulp-watch');
+const runSequence = require('run-sequence');
 
 const { BUILD_BASE, STYLES_FILES_ALL, TPL_FILES_ALL } = config;
 
@@ -11,6 +13,8 @@ gulp.task('serve', ['prepare'], () => {
         }
     })
 
-    gulp.watch(STYLES_FILES_ALL, ['sass']);
-    gulp.watch(TPL_FILES_ALL, ['tpl']);
+    const watch = (glob, tasks) => gwatch(glob, () => runSequence(...tasks));
+
+    watch(STYLES_FILES_ALL, ['sass']);
+    watch(TPL_FILES_ALL, ['tpl']);
 });
