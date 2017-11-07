70b296a4a667d9d5b3b2db4615845cc4c29f4633
Sergey Ryabov <sergey.ryabov@actum.cz>
2017-11-07 07:52:55 +0100

    Update tpl gulp task

--- gulp/tasks/tpl.js
+++ gulp/tasks/tpl.js
@@ -4,12 +4,15 @@ const nunjucks = require('gulp-nunjucks');
 const rename = require('gulp-rename');
 const browserSync = require('browser-sync');
 const plumber = require('gulp-plumber');
+const gutil = require('gulp-util');
 
 const { BUILD_BASE, DEV_BASE, TPL_FILES } = config;
 
 gulp.task('tpl-compile', () =>
     gulp.src(TPL_FILES)
-        .pipe(plumber())
+        .pipe(plumber({
+            errorHandler: e => gutil.log(gutil.colors.red(`${e.name} in ${e.plugin}: ${e.message}`))
+        }))
         .pipe(nunjucks.compile())
         .pipe(rename((path) => {
             path.extname = '.html'
