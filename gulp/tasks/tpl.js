a5c3eee989e141d1f03bca94f6f516e10b7c06fc
Sergey Ryabov <sergey.ryabov@actum.cz>
2017-11-07 21:49:47 +0100

    Improve tpl compilation task

--- gulp/tasks/tpl.js
+++ gulp/tasks/tpl.js
@@ -1,21 +1,20 @@
 const config = require('../config');
 const gulp = require('gulp');
-const nunjucks = require('gulp-nunjucks');
+const nunjucks = require('gulp-nunjucks-render');
 const rename = require('gulp-rename');
 const browserSync = require('browser-sync');
 const plumber = require('gulp-plumber');
 const gutil = require('gulp-util');
 
-const { BUILD_BASE, DEV_BASE, TPL_FILES } = config;
+const { BUILD_BASE, DEV_BASE, TPL_FILES, TPL_BASE } = config;
 
 gulp.task('tpl-compile', () =>
     gulp.src(TPL_FILES)
         .pipe(plumber({
             errorHandler: e => gutil.log(gutil.colors.red(`${e.name} in ${e.plugin}: ${e.message}`))
         }))
-        .pipe(nunjucks.compile())
-        .pipe(rename((path) => {
-            path.extname = '.html'
+        .pipe(nunjucks({
+          path: [`${TPL_BASE}/`]
         }))
         .pipe(gulp.dest(BUILD_BASE))
 );
